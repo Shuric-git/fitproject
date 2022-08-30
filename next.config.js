70e16a1ebf64be82ae6ca1ac243572b1dfd154c1
Shuric-git <ulme@rambler.ru>
2022-08-30 13:50:17 +0300

    change next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,35 @@
-const pathPrefix = process.env.NODE_ENV === 'production'
-  ? '/fitproject'
-  : '';
-
+// const pathPrefix = process.env.NODE_ENV === 'production'
+//   ? '/fitproject'
+//   : '';
+//
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     loader: "akamai",
+//     path: "/",
+//   },
+//   basePath: '/fitproject',
+//   env: {
+//     pathPrefix,
+//   },
+//   assetPrefix: '/fitproject/',
+//   experimental: {
+//     // Enables the styled-components SWC transform
+//     styledComponents: true,
+//   },
+//   eslint: {
+//     dirs: [
+//       "pages",
+//       "utils",
+//       "common",
+//       "components",
+//       "containers",
+//       "models",
+//       "services",
+//       "stories",
+//     ], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+//   },
+// }
 module.exports = {
   reactStrictMode: true,
   images: {
@@ -9,10 +37,7 @@ module.exports = {
     path: "/",
   },
   basePath: '/fitproject',
-  env: {
-    pathPrefix,
-  },
-  assetPrefix: '/fitproject/',
+  assetPrefix: '/fitproject',
   experimental: {
     // Enables the styled-components SWC transform
     styledComponents: true,
